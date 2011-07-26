a86a12289a6d765c8933dea03e8a8d2dbc757c7e
Jacob Hodes <jacob@peashutop.com>
2011-07-25 21:19:19 -0400

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vortex::Application.config.secret_token = 'cc6b9a3a313b204b9e89bdfd677df0063d1c68cbcc430b5efccdeb2e629d198aa2dfaa891f6450afd665e002d5977938b145af31c50db8d07b6a1f93ed2fa116'
